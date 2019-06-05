dd6a7af9c8cc322b2dd75d2bb4f8b67f04ea3631
imlifeilong <13772032410@163.com>
2019-06-05 21:53:03 +0800

    登陆页面

--- account/urls.py
+++ account/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-import account
+from django.urls import path
+from account import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
+    path('login/', views.login),
+    path('enroll/', views.enroll),
+    path('logout/', views.logout),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def login(request):
+
+    return render(request, 'account/login.html')
+    # return HttpResponse('login')
+
+
+def enroll(request):
+    return HttpResponse('enroll')
+
+
+def logout(request):
+    return HttpResponse('logout')
